6a35a71184d65c006fe6f672a4a87363dea7bb12
mohseno2002 <mohseno2002@gmail.com>
2026-08-14 11:25:23 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* منظومة تقنين الأراضى المنزرعة — Service Worker */
-var VERSION = "taqneen-v1.22";
+var VERSION = "taqneen-v1.24";
 var CORE = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function (e) {
